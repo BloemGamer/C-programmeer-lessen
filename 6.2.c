470ec6bb206131f04f142792c75b237bfef67b4b
Sem <sem@ie-design.nl>
2024-10-14 20:02:57 +0200

    changed a spelling mistake

--- 6.2.c
+++ 6.2.c
@@ -12,12 +12,9 @@ double pyth();
 
 int main(void)
 {
-
-
     struct punt p1, p2;
-    
 
-    printf("Give a x and y coordinate for point 1 to 5\n");
+    printf("Give a x and y coordinate for point 1 and 2\n");
     printf("point 1\n");
     scanf("%lf %lf", &p1.x, &p1.y);
     printf("point 2\n");
